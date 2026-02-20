1e359afede783f5152f3c1216948b743f61f3277
Rishabhgupta-2611 <rishabh9807114498@gmail.com>
2026-02-20 16:36:35 +0530

    [Rishabh] Add.UC2 implemented - Feet and Inches equality feature added

--- src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
+++ src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
@@ -8,7 +8,13 @@ public class QuantityMeasurementApp {
 		return f1.equals(f2);
 	}
 
+	public static boolean compareInches(double a, double b) {
+		return new Inches(a).equals(new Inches(b));
+	}
+
 	public static void main(String[] args) {
 		System.out.println("Feet equal: " + compareFeet(1.0, 1.0));
+
+		System.out.println("Inches equal: " + compareInches(12.0, 12.0));
 	}
 }

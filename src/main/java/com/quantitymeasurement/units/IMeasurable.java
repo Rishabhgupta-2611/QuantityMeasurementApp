d9de9b510626d847dbfdd191f9060f0d98fdbcd7
Rishabhgupta-2611 <rishabh9807114498@gmail.com>
2026-04-16 13:00:21 +0530

    [Rishabh] Add.JWT and Google Authnetication

---
+++ src/main/java/com/quantitymeasurement/units/IMeasurable.java
@@ -0,0 +1,26 @@
+package com.quantitymeasurement.units;
+
+@FunctionalInterface
+interface SupportsArithmetic{
+    boolean isSupported();
+}
+
+public interface IMeasurable {
+
+    SupportsArithmetic supportsArithmetic=()->true;
+
+    String getUnitName();
+
+    double getConversionFactor();
+
+    double convertToBaseUnit(double value);
+
+    double convertFromBaseUnit(double baseValue);
+
+    default boolean supportsArithmetic(){
+        return supportsArithmetic.isSupported();
+    }
+
+    default void validateOperationSupport(String operation){
+    }
+}
